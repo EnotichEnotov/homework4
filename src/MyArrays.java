569efbb6976ed10af6194504586bec2e6f672f65
Enotich Enotov <enotichenotov195@gmail.com>
2022-10-07 20:29:52 +0300

    STATIC STATIC STATIC and strange if

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class MyArrays {
-    public static double AvgOf4Digit(int[] q) {
-        double sm = 0;
-        int k = 0;
+    public static int[] MinToBegin(int[] q) {
+        int mn = q[0];
+        int c;
+        int a;
         for (int i = 0; i < q.length; ++i){
-            if (q[i] >= 1000 && q[i] <= 9999) {
-                sm += q[i];
-                k += 1;
+            if (mn > q[i]){
+                mn = q[i];
             }
+            //c = q[i];
+            //q[i+1] = c;
         }
-        if (k == 0){
-            return -1.00;
+        c = q[0];
+        for (int i = 0; i < q.length; ++i){
+            if (q[i] == mn) {
+                a = q[i];
+                q[i] = c;
+                c = a;
+                break;
+            }
+            a = q[i];
+            q[i] = c;
+            c = a;
+
         }
-        return sm/ k;
+        q[0] = mn;
+        return q;
     }
 
 
         public static void main(String[] args) {
+
             Scanner in = new Scanner(System.in).useLocale(Locale.ENGLISH);
             int n = in.nextInt();
             int[] ar = new int[n];
             for (int i = 0; i < n; ++i){
                 ar[i] = in.nextInt();
             }
-            System.out.println(AvgOf4Digit(ar));
+            ar = (MinToBegin(ar));
+            if (ar.length == 0){//Немного не понял про "Если в массиве нет таких чисел, метод должен вернуть число -1", т.к минимум всегда есть, на всякий случай написал условие
+                System.out.println(-1);
+            }
+            else {
+                for (int i = 0; i < n; ++i) {
+                    System.out.print(ar[i] + " ");
+                }
+            }
         }
     }
+
